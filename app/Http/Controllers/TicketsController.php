5d660bf23c78f5472e93a4ecc9e17148aa537caf
unknown <milancez@gmail.com>
2018-02-11 22:34:45 +0100

    Create tickets migrations(table), Ticket model, controller and view

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TicketsController extends Controller
+{
+    public function create() {
+      return view('tickets.create');
+    }
+}
